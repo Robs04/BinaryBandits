6161ab1a51afda656dde5bbfff1c91eeb6efcb14
Orell S <orell.acc@pm.me>
2025-03-20 13:00:52 +0100

    make backend accessable from html files

--- api/app.py
+++ api/app.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template
-from models import Person
+from sampleData import *
 
 app = Flask(__name__)
 
-# Sample data
-people = [
-    Person("Alice", 300000),
-    Person("Bob", 250000),
-    Person("Charlie", 10000)
-]
-
 @app.route('/')
 def index():
-    return render_template("index.html", people=people)
+    return render_template("/HTML/Clients/client1_portfolio_overview.html", people=people)
 
 if __name__ == '__main__':
     app.run(debug=True)
