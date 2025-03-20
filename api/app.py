36d95e6cac79af6cd9eeef54bc00b0bf67eb0517
Christian <christian.steiner367@gmail.com>
2025-03-20 13:29:56 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -8,4 +8,4 @@ def index():
     return render_template("/HTML/Clients/client1_portfolio_overview.html", people=people)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
